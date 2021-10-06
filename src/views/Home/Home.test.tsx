42bb26908900fc9b05eb2df2ba3e134ea29ebd8c
Stoil Stoychev <stoilstoychev@gmail.com>
2021-10-06 18:14:45 +0300

    Add unit test

--- src/views/Home/Home.test.tsx
+++ src/views/Home/Home.test.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, waitFor} from '@testing-library/react';
 import Home from './Home';
 
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const getProjectResponse = {
+    "has_more": true,
+    "data": [
+        {
+            "id": "ljmkOq7vXd239gAEmALWQ8ZGVD5ExNzr",
+            "name": "Larimar Wind",
+        },
+        {
+            "id": "gMbvJoOaX54V1wpNwA8dWDGQ7m239Bx0",
+            "name": "Southern Cardamom",
+        },
+        {
+            "id": "va1BER4JZqnzPkYxZALg0GeQDoXlWO5x",
+            "name": "Kootznoowoo",
+        },
+        {
+            "id": "BmWxrvXo29eGqzA1eANL5PwnkgaO8R31",
+            "name": "Alligator River",
+        },
+        {
+            "id": "MXJvWdBbNaDVokYzayP9lZG6zwj7K1R2",
+            "name": "Hudson Farm",
+        }
+    ]
+}
+
+jest.mock(`endpoints/api`, () => ({
+    getProjects: () => Promise.resolve({
+        data: getProjectResponse
+    })
+}))
+
+test('Renders expected projects list', async () => {
+    const comp = render(<Home/>);
+    for (const p of getProjectResponse.data) {
+        await waitFor(() => expect(comp.getByText(p.name)).toBeInTheDocument())
+    }
 });
